4904c19b27b05e4adbb5e935c67d84766aa5b455
Bekhan <bekhan2093@mail.ru>
2022-12-20 20:53:11 +0300

    Вывел максималное число

--- Homework1/Taks2/Program.cs
+++ Homework1/Taks2/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a5 = 5;
+int a2 = 2;
+int a9 = 9; 
+int b7 = 7;
+int b10 = 10;
+int b3 = 3;
+
+int max = a5;
+if(a2> max ) max = a2;
+if(a9 > max ) max = a9;
+if(b7 > max ) max = b7;
+if(b10 > max)  max = b10;
+if(b3 > max ) max = b3;
+
+Console.WriteLine(max);
