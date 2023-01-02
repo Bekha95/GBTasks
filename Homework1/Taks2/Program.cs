320a40bd41b2eea5288a74286cf85f735fc6cf9f
Bekhan <bekhan2093@mail.ru>
2023-01-02 14:00:38 +0300

    Zadacha 3

--- Homework1/Taks2/Program.cs
+++ Homework1/Taks2/Program.cs
@@ -1,15 +1,9 @@
-﻿int a5 = 5;
-int a2 = 2;
-int a9 = 9; 
-int b7 = 7;
-int b10 = 10;
-int b3 = 3;
+﻿using static System.Console;
+int a1 = 44;
+int b2 = 5;
+int c3 = 78;
 
-int max = a5;
-if(a2> max ) max = a2;
-if(a9 > max ) max = a9;
-if(b7 > max ) max = b7;
-if(b10 > max)  max = b10;
-if(b3 > max ) max = b3;
-
-Console.WriteLine(max);
+int max = a1;
+if(b2 > max) max = b2;
+if(c3 > max) max = c3;
+WriteLine(max);
